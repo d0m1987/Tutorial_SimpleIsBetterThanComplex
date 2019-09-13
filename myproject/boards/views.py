f21f5869c9c19aaea36e09ff8d3869b7c3c9cf74
DOMINIC BÜHLER <dominic@DOMINICBP15Zoll.home>
2019-09-13 20:32:51 +0200

    Added tests for board topics view

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
+from django.http import Http404
 from .models import Board
 
 def home(request):
@@ -11,6 +12,8 @@ def home(request):
     return render(request, 'home.html', {'boards':boards})
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
-    
+    try:
+        board = Board.objects.get(pk=pk)
+    except:
+        raise Http404
     return render(request, 'topics.html', {'board': board})
