5c981a5e222072b9d3c7ab8093b296b336ea8dc9
DOMINIC BÜHLER <dominic@DOMINICs-MacBook-Pro-15-Zoll.local>
2019-09-14 12:08:09 +0200

    Added new topic route + view + template

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,6 +20,7 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('boards/<str:pk>/', views.board_topics, name='board_topics'),
+    path('boards/<str:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
     
 ]
