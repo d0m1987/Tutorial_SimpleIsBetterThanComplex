fe9e9b583c422a33da0c024c4989166c3400aad2
DOMINIC BÜHLER <dominic@DOMINICBP15Zoll.home>
2019-09-14 16:54:16 +0200

    Added django-widget-tweaks for form rendering

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'widget_tweaks',
     # self created apps
     'boards',
 ]
