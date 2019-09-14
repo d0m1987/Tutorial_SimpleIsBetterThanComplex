242c8d0654251e3975c0e750c3ef50cf385acad9
DOMINIC BÜHLER <dominic@DOMINICBP15Zoll.home>
2019-09-14 18:52:00 +0200

    Added user creation + user name in nav bar

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form':form})
