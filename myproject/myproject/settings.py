1839f69c34fb7ad7d68cbc362371f8888b20457e
DOMINIC BÜHLER <dominic@DOMINICBP15Zoll.home>
2019-09-14 20:15:21 +0200

    Integrated login

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -129,4 +129,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -23,6 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', account_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/<str:pk>/', views.board_topics, name='board_topics'),
     path('boards/<str:pk>/new/', views.new_topic, name='new_topic'),
