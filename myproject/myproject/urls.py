9f547c50acca8a00dc0dc7f9cd1c6cb8547b5edf
DOMINIC BÜHLER <dominic@DOMINICBP15Zoll.home>
2019-09-14 18:21:07 +0200

    Created "Sign Up" path

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from boards import views 
+from accounts import views as account_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup', account_views.signup, name='signup'),
     path('boards/<str:pk>/', views.board_topics, name='board_topics'),
     path('boards/<str:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
