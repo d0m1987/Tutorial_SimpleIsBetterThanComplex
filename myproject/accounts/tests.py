5eeceb282ea0efc119b1dae12a91c244769cdccd
DOMINIC BÜHLER <dominic@DOMINICBP15Zoll.home>
2019-09-14 19:36:53 +0200

    Refactoring UserCreationForm to include email; added more signup tests

--- myproject/accounts/tests.py
+++ myproject/accounts/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 from django.test import TestCase
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from .views import signup
 
 class SignUpTests(TestCase):
@@ -27,13 +27,24 @@ class SignUpTests(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
 
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+        
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'john@doe.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456'
         }
--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.contrib.auth import login
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form':form})
